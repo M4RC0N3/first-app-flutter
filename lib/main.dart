1c046ce0f4decd7e524a3d1571d8f88f7ec8eab3
M4RC0N3 <marcone@quickup.io>
2023-04-04 16:51:58 -0300

    widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,111 @@
 import 'package:flutter/material.dart';
 
 void main(){
-  runApp(Finger());
+  runApp(const Finger());
 }
 
 class Finger extends StatelessWidget{
+  const Finger({super.key});
+ 
   @override
-
   Widget build(BuildContext context){
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Tap'),
+          title: const Text('Tap'),
         ),
-        body: ,
-      )
-    )
+        body: Container(
+          alignment: Alignment.center,
+          child: Container(
+            margin: const EdgeInsets.fromLTRB(40, 0, 40, 0),
+            constraints: const BoxConstraints(maxWidth: 500),
+            
+            child: Column(
+              children: [
+              
+                Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        constraints: const BoxConstraints(maxHeight: 500),
+                        child: Image.asset('assets/images/igreja.jpg'),
+                      )
+                    )
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: const[
+                              Text('Igreja da praça',
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                              Text('Porto Alegre, Rio grande do Sul'),
+                            ]
+                          ),
+                          Column(
+                            children: [
+                                Icon(
+                                Icons.star,
+                                color: Colors.red[500],
+                              ),
+                              const Text('41'), 
+                            ],
+                          )
+                        
+                      
+                    
+                  ],
+                ),
+                Row(),
+                Row(),
+              ]
+            ),
+          ),
+          
+          /* children: [
+            Row(
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    
+                    child: Image.asset('assets/images/igreja.jpg'),
+                  )
+                ),
+              ],
+            ),
+
+            Row(
+              children: [
+                Column(
+                  children: [
+                    Row(
+                      children:  const[
+                        Text('Farol da Barra'),
+                      ],
+                    ),
+                    Row(
+                      children: const[
+                        Text('Salvador, BA'),
+                      ],
+                    )
+                  ],
+                ),
+                Column(
+                  children: const [
+                    Icon(Icons.star, color: Colors.green),
+                    Text('31')
+                  ],
+                )
+              ],
+            )
+          ], */
+        ),
+      ),
+    );
   }
 }
